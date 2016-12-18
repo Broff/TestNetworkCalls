132323bcfa904d3db88a17574e58972a67157f57
broff <dvd2444@mail.ru>
2016-12-18 15:01:59 +0200

    Add core

---
+++ Assets/project/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField]
+    private Transform playerTransform;
+    [SerializeField]
+    private float yDlt;
+    [SerializeField]
+    private float speed;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 dir = new Vector3(playerTransform.position.x, playerTransform.position.y + yDlt, -10);
+        transform.position = Vector3.Lerp(transform.position, dir, Time.deltaTime * speed);
+	}
+}
---
+++ Assets/project/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerController : NetworkBehaviour {
+    
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float jumpSpeed;
+
+    private bool isGround = false;
+
+    private Rigidbody2D body;
+    private SpriteRenderer sprite;
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            body = GetComponent<Rigidbody2D>();
+            sprite = GetComponentInChildren<SpriteRenderer>();
+            sprite.color = Color.blue;
+        }
+    }
+
+	void Update () {
+        
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (Input.GetButton("Horizontal"))
+        {
+            Run();
+        }
+
+        if (Input.GetButtonDown("Vertical"))
+        {
+            Jump();
+        }        
+	}
+
+    void FixedUpdate()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        //if (isLocalPlayer)
+        {
+            IsGround();
+        }
+    }
+
+    private void Run()
+    {
+        Vector3 direction = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, Time.deltaTime * moveSpeed);
+        sprite.flipX = direction.x < 0.0f;
+    }
+
+    private void Jump()
+    {
+        if (isGround)
+        {
+            body.AddForce(transform.up * jumpSpeed, ForceMode2D.Impulse);
+            isGround = false;   
+        }
+    }
+
+    private void IsGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.2f);
+        isGround = colliders.Length > 1;
+    }
+}
